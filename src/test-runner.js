844c2f6f63be75cbbae1f3d90f20ab359058912e
Jordan <jordan@smartdust.me>
2024-10-31 15:08:47 +0100

    Add sending message when test stopped failing

--- src/test-runner.js
+++ src/test-runner.js
@@ -7,11 +7,11 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
-const dateFilePath = path.join(__dirname, 'lastReportedDate.txt');
+const logFilePath = path.join(__dirname, 'e2e-tests.log');
 
 function isTodayAlreadyReported() {
   try {
-    const lastReportedDate = fs.readFileSync(dateFilePath, 'utf-8');
+    const lastReportedDate = fs.readFileSync(logFilePath, 'utf-8').split(" ")[0]
     const today = new Date().toISOString().slice(0, 10); // Get today's date in YYYY-MM-DD format
     return lastReportedDate === today;
   } catch (error) {
@@ -20,9 +20,23 @@ function isTodayAlreadyReported() {
   }
 }
 
-function markTodayAsReported() {
+function hasLastTestFailed() {
+  try {
+    const passInfo = fs.readFileSync(logFilePath, 'utf-8').split(" ")[1].trim()
+    console.log(passInfo)
+    return passInfo === 'failed'.trim();
+  } catch (error) {
+    console.error(error);
+    // If the file doesn't exist or an error occurs, assume it's not reported yet
+    return false;
+  }
+}
+
+function logTestRun(passed) {
   const today = new Date().toISOString().slice(0, 10);
-  fs.writeFileSync(dateFilePath, today, 'utf-8');
+  const passInfo = passed ? 'passed' : 'failed';
+  const logMessage = `${today} ${passInfo}`;
+  fs.writeFileSync(logFilePath, logMessage, 'utf-8');
 }
 
 dns.resolve('www.google.com', function (err) {
@@ -31,22 +45,20 @@ dns.resolve('www.google.com', function (err) {
   } else {
     cypress.run({
       reporter: 'junit',
-      browser: 'firefox',
+      browser: 'electron',
     }).then(async (results) => {
       if (results.totalPassed !== results.totalTests) {
         await sendSlackMessage('Google login on smartdust.me test failed!', process.env.SLACK_CHANNEL_ID)
           .catch(e => console.error(e));
-        if (!isTodayAlreadyReported()) {
-          markTodayAsReported();
-        }
+        logTestRun(false);
       } else {
         console.log('All tests passed');
-        if (!isTodayAlreadyReported()) {
+        if (!isTodayAlreadyReported() || hasLastTestFailed()) {
           await sendSlackMessage('Wordpress E2E tests passed', process.env.SLACK_CHANNEL_ID);
-          markTodayAsReported();
         } else {
           console.log('Slack message already sent today.');
         }
+        logTestRun(true);
       }
     })
   }
